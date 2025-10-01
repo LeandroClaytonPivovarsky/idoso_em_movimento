b6f573537b6a989834a7d0f4d7a6da3193a4330c
Leandro Clayton Pivovarsky <leandro123clayton@gmail.com>
2025-10-01 08:44:27 -0300

    Starting project. Creating files for clean architecture

---
+++ android/app/src/main/kotlin/com/example/idoso_em_movimento/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.idoso_em_movimento
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/src/features/event/domain/entities/event_entity.dart
@@ -0,0 +1,42 @@
+import 'package:equatable/equatable.dart';
+
+class EventEntity extends Equatable {
+  final String id;
+  final String title;
+  final String date;
+  final String time;
+  final String location;
+  final String category;
+  final String imageUrl;
+  final int attendees;
+  final int maxAttendees;
+  final String description;
+
+  const EventEntity({
+    required this.id,
+    required this.title,
+    required this.date,
+    required this.time,
+    required this.location,
+    required this.category,
+    required this.imageUrl,
+    required this.attendees,
+    required this.maxAttendees,
+    required this.description,
+  });
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [
+        id,
+        title,
+        date,
+        time,
+        location,
+        category,
+        imageUrl,
+        attendees,
+        maxAttendees,
+        description
+      ];
+}
